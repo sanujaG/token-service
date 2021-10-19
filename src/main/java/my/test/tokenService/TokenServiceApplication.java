f5dbb87285ccaeac79d7f813dc667b79b34b8512
sanujaG <sanu.gamage@gmail.com>
2021-10-19 17:25:40 +0530

    Update TokenServiceApplication.java

--- src/main/java/my/test/tokenService/TokenServiceApplication.java
+++ src/main/java/my/test/tokenService/TokenServiceApplication.java
@@ -1,4 +1,4 @@
-package net.epiclanka.tokenService;
+package my.test.tokenService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
