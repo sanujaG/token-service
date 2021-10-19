ca57c2d69de6f59568a295cdad25afab60c2e4ff
sanujaG <sanu.gamage@gmail.com>
2021-10-19 17:21:39 +0530

    Initial commit

---
+++ src/main/java/net/epiclanka/tokenService/TokenServiceApplication.java
@@ -0,0 +1,18 @@
+package net.epiclanka.tokenService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author Sanuja_G
+ *   @CreatedTime on 19-10-2021 04:32:22 PM
+ */
+
+@SpringBootApplication
+public class TokenServiceApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(TokenServiceApplication.class, args);
+    }
+}
